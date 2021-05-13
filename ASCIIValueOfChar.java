ea656f513b66084f3c61b799c9acaff8e811e5b5
vijaytripathi553 <68139428+vijaytripathi553@users.noreply.github.com>
2021-05-13 17:18:58 +0530

    Create ASCIIValueOfChar.java

---
+++ ASCIIValueOfChar.java
@@ -0,0 +1,12 @@
+package com.string.interview_programs;
+
+public class ASCIIValueOfChar {
+
+
+	public static void main(String[] args) {
+		char h='A';
+		System.out.println("ASCII Value of "+h+"is:"+(int)h);
+
+	}
+
+}
