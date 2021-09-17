94a06ce744ade5edbb8856b7edca08f570cea747
vijaytripathi553 <vijaytripathi553@gmail.com>
2021-09-17 09:41:00 +0530

    Modified Convert String Array To List

---
+++ ConvertStringArrayToList.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ConvertStringArrayToList {
+	public static void main(String[] args) {
+		
+		String s1[]= {"ABC","XYZ","PQR"};
+		
+		List<String> list=Arrays.asList(s1);
+		
+		System.out.println(list);
+		
+	}
+
+}
