067babed83a1d43e5be8ec9f5ba785256ae3ca49
vijaytripathi553 <vijaytripathi553@gmail.com>
2021-09-28 09:57:13 +0530

    ReverseStringThrogh reverse() fun

---
+++ ReverseStringThroughReverseFunc.java
@@ -0,0 +1,13 @@
+
+public class ReverseStringThroughReverseFunc {
+	public static void main(String[] args) {
+		
+		StringBuilder ab=new StringBuilder("Vijay");
+		
+		
+		
+		System.out.println("Reverse String is :"+" "+ab.reverse());
+		
+	}
+
+}
