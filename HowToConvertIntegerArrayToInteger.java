516956db2a8a2664c9180d7928d358ac91fbfb5f
vijaytripathi553 <vijaytripathi553@gmail.com>
2022-06-13 13:03:20 +0530

    Adding Program which is realred to converting an integer array to int on 10th June 2022

--- HowToConvertIntegerArrayToInteger.java
+++ HowToConvertIntegerArrayToInteger.java
@@ -1,5 +1,4 @@
 package com.live.interview;
-
 import java.util.Arrays;
 
 /* ✅ In this Program Will convert an int array (array1) to int
@@ -7,8 +6,8 @@ import java.util.Arrays;
  *   OUTPUT:100200300400
  *   
  *   Approach :
- * STEP 1:   In order to convert an Integer array to int first let's convert an int array to String 
- * STEP 2: After Converting t
+ * STEP 1:   In order to convert an Integer array to int first let's convert an int array to String with the help of Arrays.toString().  Same could have been done by using for loop and adding each element to the list by + or concat() method.
+ * STEP 2: After Converting the Array to String, now we can convert this String to int.
  */
 
 public class HowToConvertIntegerArrayToInteger {
@@ -16,19 +15,65 @@ public class HowToConvertIntegerArrayToInteger {
 // Method to Convert an integer array to integer
 	private static int convertAnIntegerArrayToInteger(int[] array1) 
 	{
+		
 		String ans=Arrays.toString(array1);
-		System.out.println("Converted an Integer Array to Integer and here is array:"+ans);
+		System.out.println("Converted an Integer Array to String and here is array:"+ans);
 		
 		System.out.println("👍🏻 Now, lets remove the comma from the string");
-		ans=ans.replaceAll("\\D", "");  // Here, //D is nothing but metachacter which is used to remove the Non-digit characters
+		ans=ans.replaceAll("\\D", "");  // Here, //D is nothing but metacharcter which is used to remove the Non-digit characters
 		System.out.println("After removing comma from the string:"+ans);
+		System.out.println("👌 Ultimately, Let's convert this string to integer and return it");
 		
-		
-		return 0;
+		int fromStringToInt=0;
+		// Below code is to handle the NumberFormatException which occurs when we try to convert string to Integer (Other then No). Other then No in the sense String s1="123"  can be converted to integer but String s2="Vijay" will raise NumberFormatException.
+		try
+		{
+		//	It is a best practice to check if the passed-in string is numeric or not before trying to convert it to integer.
+		// For tht i have created one method to check whether it is numeric
+			boolean checkNumeric=isNumeric(ans);
+			
+			fromStringToInt=Integer.valueOf(ans); 	// String s1="123" will be converted to String . 
+		}
+		catch(NumberFormatException nfe)
+		{
+			nfe.printStackTrace();  // This is the method coming from Throwable class which tells you what happened and where in the code this happened.
+			
+			if(ans==null || ans.isEmpty())       // To check whether string is empty or not
+			{
+				System.out.println("Your String is null or your input is string:");
+			}
+			else if(ans.matches("^[a-zA-z]*$"))  // To check whether string contains alphanumeric character
+			{
+				System.out.println("Your string contains alphanuanric chacter which causes NumberFormatException");
+			}
+			//else if()
+			/*
+			 * else { return fromStringToInt; }
+			 */
+			
+		}
+	
+		return fromStringToInt;
 	}
+	
+	
+	// Method to check whether string contains numeric or not
+	
+	private static boolean isNumeric(String ans)
+	{
+		// Using String matchers() method will check whether it contains numeric or not
+		if(ans.matches("[0-9.]+"))
+		{
+			return true;
+		}
+		else
+			return false;
+		
+	}
+	
 	// Main Method
 	public static void main(String[] args) {
-		int array1[]=new int[] {100,200,300,400};
+		int array1[]=new int[] {100,200,300};
 		
 		int result=convertAnIntegerArrayToInteger(array1);
 		System.out.println(result);
